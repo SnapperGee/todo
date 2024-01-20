35e1e753bad2f0aba3c78e5a22ca2f02dd83ca72
Snap <XxSnapperGeexX@gmail.com>
2024-01-20 00:03:15 -0800

    Move dev debug line for db connection into connection module.

--- server/src/main/ts/connection.ts
+++ server/src/main/ts/connection.ts
@@ -3,4 +3,11 @@ import "dotenv/config";
 
 mongoose.connect(`mongodb://${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
 
-export default mongoose.connection;
+export const dbConnection = mongoose.connection;
+
+if (process.env.NODE_ENV === "development")
+{
+    console.log(`DB connected to: ${dbConnection.host}:${dbConnection.port}/${dbConnection.name}`);
+}
+
+export default dbConnection;
--- server/src/main/ts/index.ts
+++ server/src/main/ts/index.ts
@@ -6,8 +6,7 @@ db.once("open", () => {
     {
         app.listen(process.env.PORT, () =>
         {
-            console.log(
-                `DB connected to: ${db.host}:${db.port}/${db.name}\nExpress server listening on http://127.0.0.1:${process.env.PORT}`);
+            console.log(`Express server listening on http://127.0.0.1:${process.env.PORT}`);
         });
     }
     else
