80aa513ca6e747edda98bb974e85cd07b1f24167
Snap <XxSnapperGeexX@gmail.com>
2024-01-19 23:56:01 -0800

    Connect to db in server index entry point.

--- server/src/main/ts/index.ts
+++ server/src/main/ts/index.ts
@@ -1,13 +1,17 @@
 import { app } from "./server.js";
+import db from "./connection.js";
 
-if (process.env.NODE_ENV === "development")
-{
-    app.listen(process.env.PORT, () =>
+db.once("open", () => {
+    if (process.env.NODE_ENV === "development")
     {
-        console.log(`Express server listening on http://localhost:${process.env.PORT}`);
-    });
-}
-else
-{
-    app.listen(process.env.PORT);
-}
+        app.listen(process.env.PORT, () =>
+        {
+            console.log(
+                `DB connected to: ${db.host}:${db.port}/${db.name}\nExpress server listening on http://127.0.0.1:${process.env.PORT}`);
+        });
+    }
+    else
+    {
+        app.listen(process.env.PORT);
+    }
+});
