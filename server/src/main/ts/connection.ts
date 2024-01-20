810e6d3b7a21b3e0d414f3eff4110232e005f190
Snap <XxSnapperGeexX@gmail.com>
2024-01-20 00:39:36 -0800

    Move db connection print statement from connection module into index module.

--- server/src/main/ts/connection.ts
+++ server/src/main/ts/connection.ts
@@ -5,9 +5,4 @@ mongoose.connect(`mongodb://${process.env.HOST}:${process.env.DB_PORT}/${process
 
 export const dbConnection = mongoose.connection;
 
-if (process.env.NODE_ENV === "development")
-{
-    console.log(`DB connected to: ${dbConnection.host}:${dbConnection.port}/${dbConnection.name}`);
-}
-
 export default dbConnection;
