059f50d9b9eea74c5ed7b7397fbf7e55b1467998
Snap <XxSnapperGeexX@gmail.com>
2024-01-20 21:50:55 -0800

    Add more users to seed script.

--- server/scripts/users.ts
+++ server/scripts/users.ts
@@ -4,7 +4,7 @@ export const users = [
     {
         _id: new Types.ObjectId(),
         username: "JohnWick",
-        password: "johnwickpassword",
+        password: "johnwick",
         tasks: [] as Record<string, unknown>[]
     },
     {
@@ -35,6 +35,48 @@ export const users = [
         _id: new Types.ObjectId(),
         username: "RogerSmith",
         password: "rogersmith",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "JohnnyBravo",
+        password: "johnnybravo",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "Dexter",
+        password: "dexterslab",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "HankHill",
+        password: "hankhill",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "Pim",
+        password: "pimpassword",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "Meatwad",
+        password: "meatwadpassword",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "Batman",
+        password: "batmanpassword",
+        tasks: [] as Record<string, unknown>[]
+    },
+    {
+        _id: new Types.ObjectId(),
+        username: "Superman",
+        password: "superman",
         tasks: [] as Record<string, unknown>[]
     }
 ];
