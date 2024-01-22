bb3e4da8045f0fee9f15dca774ed4a266c1e3aee
Snap <XxSnapperGeexX@gmail.com>
2024-01-22 13:36:31 -0800

    Remove unused import from resolvers module.

--- server/src/main/ts/graphql/resolvers.ts
+++ server/src/main/ts/graphql/resolvers.ts
@@ -1,7 +1,23 @@
-export const resolvers = {
+import { User } from "../model/user.js";
+import { Task } from "../model/task.js";
+import { ISubtask } from "../model/subtask.js";
+import { Types } from "mongoose";
+
+export const resolvers =
+{
     Query:
     {
+        user: async (_parent: unknown, {id}: {id: string}): Promise<typeof User | null> =>
+            await User.findById(id),
 
+        task: async (_parent: unknown, {id}: {id: string}): Promise<typeof Task | null> =>
+            await Task.findById(id),
+
+        subtask: async (_parent: unknown, {userId: taskId, subtaskId}: {userId: string, subtaskId: string}): Promise<ISubtask | null | undefined> =>
+            (await Task.findById(taskId))?.subtasks.id(subtaskId),
+
+        subtasks: async (_parent: unknown, {id}: {id: string}): Promise<Types.DocumentArray<ISubtask> | undefined> =>
+            (await Task.findById(id))?.subtasks
     },
 
     Mutation:
