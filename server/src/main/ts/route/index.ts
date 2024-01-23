176603467ed13138186a3fc251346b977130db1b
Snap <XxSnapperGeexX@gmail.com>
2024-01-22 17:16:53 -0800

    Add filler api get route to server.

--- server/src/main/ts/route/index.ts
+++ server/src/main/ts/route/index.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 
 export const apiRoute = Router();
 
-apiRoute.get("/api");
+apiRoute.get("/api", (_req, res) => res.send("<h1>API Route hit</h1>"));
 
 export default apiRoute;
