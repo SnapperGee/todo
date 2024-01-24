f02501127021cdeea548a2fdbe0a0b2c9f83bb9e
Snap <XxSnapperGeexX@gmail.com>
2024-01-23 21:21:08 -0800

    Add authMiddleware and signToken functions to auth module.

--- server/src/main/ts/auth.ts
+++ server/src/main/ts/auth.ts
@@ -1,1 +1,38 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export interface RequestWithUserPayload extends Request
+{
+    user: string | JwtPayload;
+}
+
+const authMiddleWare = (req: RequestWithUserPayload, res: Response, next: NextFunction) =>
+{
+    // allows token to be sent via req.body, req.query, or headers
+    const token = req.headers.authorization?.substring("Bearer ".length);
+
+    if ( ! token)
+    {
+      return req;
+    }
+
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+    try
+    {
+      req.user = jwt.verify(token, process.env.JWT_SECRET!);
+      next();
+    }
+    catch(error)
+    {
+      console.error("%s\n\nAuth error", error);
+    }
+
+    // return the request object so it can be passed to the resolver as `context`
+    return req;
+};
+
+const signToken = (_id: string, username: string) =>
+{
+    const user = { _id, username };
+    return jwt.sign({ user }, process.env.JWT_SECRET!);
+};
