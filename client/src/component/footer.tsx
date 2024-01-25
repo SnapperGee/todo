bd0551982cc97224c93a7dd8cd9908c1f678cd7d
Danny Collan <collan97@gmail.com>
2024-01-24 23:52:47 -0800

    Added navbar, signup and signin aesthetics, and added footer

--- client/src/component/footer.tsx
+++ client/src/component/footer.tsx
@@ -1,8 +1,41 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Link from '@mui/material/Link';
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
 export const Footer = () =>
-(
-    <>
-        <h2>Footer</h2>
-    </>
-);
+{
+    return (
+        <ThemeProvider theme={defaultTheme}>
+ <CssBaseline />
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1">
+            <Grid item xs={12}>
+            <Typography color="textSecondary" variant="subtitle1">
+              {`${new Date().getFullYear()} | React | Material UI | React Router`}
+            </Typography>
+          </Grid>
+            </Typography>
+          </Container>
+        </Box>
+    </ThemeProvider>
+    );
+};
 
   export default Footer;
