d3ce6ebfc29ab5d211a243748b272e2d11333eb6
Snap <XxSnapperGeexX@gmail.com>
2024-01-25 09:24:00 -0800

    Use graphql error in auth middleware function if jwt verify fails.

--- server/src/main/ts/auth.ts
+++ server/src/main/ts/auth.ts
@@ -1,14 +1,14 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
+import { GraphQLError } from "graphql";
+import type { Request, Response, NextFunction } from "express";
 
 export interface RequestWithUserPayload extends Request
 {
     user: string | JwtPayload;
 }
 
-export const authMiddleware = (req: RequestWithUserPayload, res: Response, next: NextFunction) =>
+export const authMiddleware = (req: RequestWithUserPayload, _res: Response, next: NextFunction) =>
 {
-    // allows token to be sent via req.body, req.query, or headers
     const token = req.headers.authorization?.substring("Bearer ".length);
 
     if ( ! token)
@@ -16,19 +16,24 @@ export const authMiddleware = (req: RequestWithUserPayload, res: Response, next:
       return req;
     }
 
-    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try
     {
-      req.user = jwt.verify(token, process.env.JWT_SECRET!);
-      next();
+        req.user = jwt.verify(token, process.env.JWT_SECRET!);
+        next();
     }
     catch(error)
     {
-      console.error("%s\n\nAuth error", error);
-      res.status(401).json({message: "Auth error"});
+        throw new GraphQLError("Auth error",
+            {
+                extensions: {
+                code: 'UNAUTHENTICATED',
+                http: { status: 401 },
+                cause: error
+                }
+            }
+        );
     }
 
-    // return the request object so it can be passed to the resolver as `context`
     return req;
 };
 
