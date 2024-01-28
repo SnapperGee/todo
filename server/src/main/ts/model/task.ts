9f2c55af56fdb1147b23e66a0f4ea3a018f0018e
Snap <XxSnapperGeexX@gmail.com>
2024-01-27 21:07:27 -0800

    Add doc comments to task module.

--- server/src/main/ts/model/task.ts
+++ server/src/main/ts/model/task.ts
@@ -1,6 +1,15 @@
+/**
+ * This module contains the Task model and its schema. A Task represents a task
+ * that a user can schedule and accomplish.
+ * @module task
+ */
+
 import { User } from "./user.js";
 import { Schema, model, Types, UpdateQuery } from "mongoose";
 
+/**
+ * The interface for the Task {@link taskSchema schema} and {@link Task model}.
+ */
 export interface ITask
 {
     user: Types.ObjectId;
@@ -9,6 +18,14 @@ export interface ITask
     schedule: Date;
 }
 
+/**
+ * The schema for the {@link Task Task model}. Contains the following fields:
+ *
+ * - user: The user that owns the task. References a User document.
+ * - title: The title string of the task.
+ * - accomplished: A boolean value that indicates whether the task has been accomplished or not.
+ * - schedule: The date when the task is scheduled to be accomplished.
+ */
 export const taskSchema = new Schema<ITask>(
     {
         user: {
@@ -44,16 +61,26 @@ export const taskSchema = new Schema<ITask>(
     }
 );
 
+// Add task to the user's tasks array when task is created.
 taskSchema.post("save", async function()
 {
     await User.findByIdAndUpdate(this.user, { $push: {tasks: this} });
 });
 
+//  Delete the task from the user's tasks array when task is deleted.
 taskSchema.pre<UpdateQuery<typeof taskSchema>>("findOneAndDelete", async function() {
     const deletedTask = await this.model.findOne(this.getFilter());
     await User.findByIdAndUpdate(deletedTask?.user, { $pull: {tasks: {_id: deletedTask?._id}} });
 });
 
+/**
+ * The Task model of the {@link taskSchema}. Contains the following fields:
+ *
+ * - user: The user that owns the task. References a User document.
+ * - title: The title string of the task.
+ * - accomplished: A boolean value that indicates whether the task has been accomplished or not.
+ * - schedule: The date when the task is scheduled to be accomplished.
+ */
 export const Task = model<ITask>("Task", taskSchema);
 
 export default Task;
