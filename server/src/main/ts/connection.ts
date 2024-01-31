71b3ed2f94a970f637f8882ad0ad3b2797dc5d60
Snap <XxSnapperGeexX@gmail.com>
2024-01-30 20:36:17 -0800

    Don't use coss-env for start and update connect string for deployment.

--- server/src/main/ts/connection.ts
+++ server/src/main/ts/connection.ts
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-mongoose.connect(`mongodb://${process.env.HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+if (process.env.NODE_ENV === "production")
+{
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DIST_HOST}:/?retryWrites=true&w=majority`);
+}
+else
+{
+    mongoose.connect(`mongodb://${process.env.DEV_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+}
 
 export const dbConnection = mongoose.connection;
 
